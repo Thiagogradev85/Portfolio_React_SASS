e18b6424bd50b10a0ec426ec8ac4ba41ae77f6ba
Thiago Gramuglia <thiago_gra@hotmail.com>
2022-09-01 12:31:41 -0300

    Update AboutContainer.jsx

--- src/components/AboutContainer.jsx
+++ src/components/AboutContainer.jsx
@@ -3,7 +3,7 @@ const AboutContainer = () => {
     <section className="about-container">
       <h2>Sobre</h2>
       <p>
-        desenvolvedor Fullstack tendo esperiencia em Front com HTML5, CSS3, SASS, TailwindCSS, Gulp e em Backend com C# (Dotne6), atuei em desenvolvimento de Apps, Web Pages (SPA) e Apis. Tenho facilidade em trabalhar em equipe e bastante vontade de aprender.
+      Desenvolvedor Fullstack tendo experiência em Front com HTML5, CSS3, SASS, TailwindCSS, Gulp e em Backend com C# (Dotne6), atuei em desenvolvimento de Apps, Web Pages (SPA) e APIs. Tenho facilidade em trabalhar em equipe e bastante vontade de aprender.
       </p>
    
     </section>
