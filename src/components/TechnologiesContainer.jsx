e8103f883c3a5ad82f0eb2dde220065d11db5fc3
Thiago Gramuglia <thiago_gra@hotmail.com>
2022-10-20 12:06:30 -0300

    Update Technologies

--- src/components/TechnologiesContainer.jsx
+++ src/components/TechnologiesContainer.jsx
@@ -9,24 +9,35 @@ import {
   DiBootstrap,
   DiDotnet,
   DiGulp,
-  DiGithubBadge
+  DiGithubBadge,
+  
 } from "react-icons/di";
+
+import {
+  SiTailwindcss,
+  SiCsharp,
+  SiJava
+
+} from "react-icons/si";
 
 import "../styles/components/technologiescontainer.sass";
 
 const technologies = [
+  { id: "SiCsharp", name: "C#", icon: <SiCsharp /> },
+  { id: "dotnet", name: "Dotnet", icon: <DiDotnet /> },
   { id: "html", name: "HTML5", icon: <DiHtml5 /> },
   { id: "css", name: "CSS3", icon: <DiCss3 /> },
-  { id: "sass", name: "SASS", icon: <DiSass /> },
-  { id: "bootstrap", name: "Bootstrap", icon: <DiBootstrap /> },
   { id: "js", name: "JavaScript", icon: <DiJsBadge /> },
+  { id: "SiJava", name: "Java", icon: <SiJava /> },
+  { id: "sass", name: "SASS", icon: <DiSass /> },
+  { id: "bootstrap", name: "Bootstrap", icon: <DiBootstrap /> },  
   { id: "node", name: "Node.js", icon: <DiNodejsSmall /> },
   { id: "mysql", name: "MySQL", icon: <DiMysql /> },
-  { id: "react", name: "React", icon: <DiReact /> },
-  { id: "dotnet", name: "Dotnet", icon: <DiDotnet /> },
+  { id: "react", name: "React", icon: <DiReact /> },  
   { id: "gulp", name: "Gulp", icon: <DiGulp /> },
-   { id: "github", name: "Github", icon: <DiGithubBadge /> },
-  
+  { id: "github", name: "Github", icon: <DiGithubBadge /> },
+  { id: "tailwindcss", name: "Tailwindcss", icon: <SiTailwindcss /> },
+ 
   
 ];
 
