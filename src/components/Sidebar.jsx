41342b725e72cc2cfef184ecc9ff2ce682555371
Thiago Gramuglia <99081257+Thiagogradev85@users.noreply.github.com>
2025-11-19 12:27:26 -0300

    a
    a

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -12,9 +12,7 @@ const Sidebar = () => {
       <p className="title">Desenvolvedor Full Stack</p>
       <SocialNetworkContainer />
       <InformationContainer />
-      <a href="https://www.linkedin.com/in/thiago-gramuglia-b0449b3a/" className="btn" >
-        Baixe Meu Linkedin em PDF
-      </a>
+     
     </aside>
   );
 };
